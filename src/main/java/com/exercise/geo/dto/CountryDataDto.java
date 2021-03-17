93e48b5e047fbdc0df5ffb468f74a9a29be8c85d
plavaggi <pablolavaggi92@gmail.com>
2021-03-17 01:07:55 -0300

    se agrega calculo de distancia

--- src/main/java/com/exercise/geo/dto/CountryDataDto.java
+++ src/main/java/com/exercise/geo/dto/CountryDataDto.java
@@ -6,24 +6,38 @@ import lombok.NoArgsConstructor;
 import java.sql.Timestamp;
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+@JsonPropertyOrder({ "Ip", "Fecha Actual", "Pais", "ISO code", "Idioma", "Moneda Local", "Hora", "Distancia Estimada" })
 @Data
 @NoArgsConstructor
 public class CountryDataDto {
-    private String nombre;
-    private String ISOcode;
-    private List<String> horaLocal;
+    @JsonProperty("Ip")
+    private String ip;
+    @JsonProperty("Pais")
+    private String pais;
+    @JsonProperty("ISO code")
+    private String isoCode;
+    @JsonProperty("Hora")
+    private List<String> hora;
+    @JsonProperty("Idioma")
     private List<String> idioma;
+    @JsonProperty("Moneda Local")
     private String monedaLocal;
+    @JsonProperty("Distancia Estimada")
     private String distanciaEstimada;
-    private Timestamp date;
+    @JsonProperty("Fecha Actual")
+    private Timestamp fechaActual;
 
-    public CountryDataDto(String nombre, String ISOcode, List<String> horaLocal, List<String> idioma, String monedaLocal, String distanciaEstimada) {
-        this.nombre = nombre;
-        this.ISOcode = ISOcode;
-        this.horaLocal = horaLocal;
+    public CountryDataDto(String ip, String pais, String isoCode, List<String> hora, List<String> idioma, String monedaLocal, String distanciaEstimada) {
+        this.ip = ip;
+    	this.pais = pais;
+        this.isoCode = isoCode;
+        this.hora = hora;
         this.idioma = idioma;
         this.monedaLocal = monedaLocal;
         this.distanciaEstimada = distanciaEstimada;
-        this.date = new Timestamp(System.currentTimeMillis());
+        this.fechaActual = new Timestamp(System.currentTimeMillis());
     }
 }
--- src/main/java/com/exercise/geo/service/impl/GeoServiceImpl.java
+++ src/main/java/com/exercise/geo/service/impl/GeoServiceImpl.java
@@ -1,61 +1,106 @@
 package com.exercise.geo.service.impl;
 
-import com.exercise.geo.dto.CountryDataDto;
-import com.exercise.geo.response.Ip2CountryResponse;
-import com.exercise.geo.response.RestCountryResponse;
-import com.exercise.geo.service.GeoService;
-import com.fasterxml.jackson.databind.JsonNode;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+import java.time.ZoneId;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
+import com.exercise.geo.dto.CountryDataDto;
+import com.exercise.geo.response.Ip2CountryResponse;
+import com.exercise.geo.response.RestCountryLanguage;
+import com.exercise.geo.response.RestCountryResponse;
+import com.exercise.geo.service.GeoService;
+import com.fasterxml.jackson.databind.JsonNode;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class GeoServiceImpl implements GeoService {
 
-    @Value("${external.endpoint.url.api.ip2Country}")
-    private String ip2CountryEndpoint;
+	@Value("${external.endpoint.url.api.ip2Country}")
+	private String ip2CountryEndpoint;
 
-    @Value("${external.endpoint.url.api.restCountry}")
-    private String restCountry;
+	@Value("${external.endpoint.url.api.restCountry}")
+	private String restCountryEndpoint;
 
-    @Value("${external.endpoint.url.api.currencyLayer}")
-    private String currencyLayer;
+	@Value("${external.endpoint.url.api.currencyLayer}")
+	private String currencyLayerEndpoint;
 
+	@Override
+	public CountryDataDto getDataByIp(String ip) {
+		Ip2CountryResponse ip2Country = getCountryByIp(ip);
+		String isoCode3 = ip2Country.getCountryCode3();
 
+		RestCountryResponse countryData = getCountryDatAByIsoCode(isoCode3);
+		String currency = countryData.getCurrencies().get(0).getCode();
 
-    @Override
-    public CountryDataDto getDataByIp(String ip) {
-        Ip2CountryResponse ip2Country = getCountryByIp(ip);
-        RestCountryResponse countryData = getCountryDatAByIsoCode(ip2Country.getCountryCode3());
-        JsonNode getExchange = getExchange(countryData.getCurrencies().get(0).getCode());
+		BigDecimal exchange = getExchange(currency);
 
-        CountryDataDto countryDataDto = new CountryDataDto();
-/*
-        String nombre, String ISOcode, List<String> horaLocal, List<String> idioma, String monedaLocal, String distanciaEstimada
-*/
+		return prepareResponse(countryData, ip, isoCode3, currency, exchange);
 
-        return countryDataDto;
-    }
+	}
 
-    public Ip2CountryResponse getCountryByIp (String ip) {
-        RestTemplate restTemplate = new RestTemplate();
-        Ip2CountryResponse ip2Country = restTemplate.getForObject(ip2CountryEndpoint.concat(ip), Ip2CountryResponse.class);
-        return ip2Country;
-    }
+	private CountryDataDto prepareResponse(RestCountryResponse countryData, String ip, String isoCode3, String currency,
+			BigDecimal exchange) {
+		String name = countryData.getTranslations().get("es");
+		List<String> horaLocal = new ArrayList<>();
+		List<String> idiomas = new ArrayList<>();
 
-    public RestCountryResponse getCountryDatAByIsoCode (String countryIsoCode) {
-        RestTemplate restTemplate = new RestTemplate();
-        RestCountryResponse response = restTemplate.getForObject(restCountry.concat(countryIsoCode), RestCountryResponse.class);
-        return response;
-    }
+		for (String usoHorario : countryData.getTimezones()) {
+			OffsetDateTime now = OffsetDateTime.now(ZoneId.of(usoHorario));
+			horaLocal.add(now.toString());
+		}
 
-    public JsonNode getExchange (String currency) {
-        RestTemplate restTemplate = new RestTemplate();
-        JsonNode response = restTemplate.getForObject(currencyLayer.concat(currency), JsonNode.class);
-        return response;
-    }
+		for (RestCountryLanguage language : countryData.getLanguages()) {
+			idiomas.add(language.getName()+" ("+language.getIso639_1()+")");
+		}
+
+		String distance = distance(countryData.getLatlng().get(0), countryData.getLatlng().get(1));
+		return new CountryDataDto(ip, name, isoCode3, horaLocal, idiomas, currency, distance);
+	}
+
+	private String distance(double lat2, double lon2) {
+		if ((-34.0 == lat2) && (-64.0 == lon2)) {
+			return String.valueOf(0);
+		} else {
+			double theta = -64.0 - lon2;
+			double dist = Math.sin(Math.toRadians(-34.0)) * Math.sin(Math.toRadians(lat2))
+					+ Math.cos(Math.toRadians(-34.0)) * Math.cos(Math.toRadians(lat2))
+							* Math.cos(Math.toRadians(theta));
+			dist = Math.acos(dist);
+			dist = Math.toDegrees(dist);
+			dist = dist * 60 * 1.1515;
+			dist = dist * 1.609344;
+			return (String.valueOf(dist));
+		}
+	}
+
+	public Ip2CountryResponse getCountryByIp(String ip) {
+		RestTemplate restTemplate = new RestTemplate();
+		Ip2CountryResponse response = restTemplate.getForObject(ip2CountryEndpoint.concat(ip),
+				Ip2CountryResponse.class);
+		return response;
+	}
+
+	public RestCountryResponse getCountryDatAByIsoCode(String countryIsoCode) {
+		RestTemplate restTemplate = new RestTemplate();
+		RestCountryResponse response = restTemplate.getForObject(restCountryEndpoint.concat(countryIsoCode),
+				RestCountryResponse.class);
+		return response;
+
+	}
+
+	public BigDecimal getExchange(String currency) {
+		RestTemplate restTemplate = new RestTemplate();
+		JsonNode response = restTemplate.getForObject(currencyLayerEndpoint.concat(currency), JsonNode.class);
+		return BigDecimal.valueOf(response.get("quotes").get("USD".concat(currency)).asDouble());
+	}
 }
